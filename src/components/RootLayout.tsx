e8d38aaa7caa18bd38fcadebd6ac06602cd1b1b7
jkarir <jurgen.karir@bluepixelsoftware.com>
2025-02-26 20:50:04 -0500

    feat: update Logo component for improved rendering; adjust RootLayout to ensure logo visibility and enhance TextInput stories with user interaction testing

--- src/components/RootLayout.tsx
+++ src/components/RootLayout.tsx
@@ -69,11 +69,7 @@ function Header({
           onMouseEnter={() => setLogoHovered(true)}
           onMouseLeave={() => setLogoHovered(false)}
         >
-          <Logomark
-            className="h-8 sm:hidden"
-            invert={invert}
-            filled={logoHovered}
-          />
+          <Logomark className="h-8 sm:hidden" invert={invert} filled={true} />
           <Logo
             className="hidden h-8 sm:block"
             invert={invert}
